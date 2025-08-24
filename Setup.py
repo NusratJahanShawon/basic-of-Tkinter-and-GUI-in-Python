d47b9c4f1ddce0027bded9516348893b4a7ec929
Nusrat Jahan Shawon <nusrat.shawona27@gmail.com>
2025-08-24 15:25:10 +0600

    row and column expanding rules

--- Setup.py
+++ Setup.py
@@ -40,6 +40,18 @@ button1.grid(row=0, column=0) #place the button in the window using grid
 button2.grid(row=1, column=0) #place the button in the window using grid
 button3.grid(row=2, column=0) #place the button in the window using grid
 
+#configure grid columns 
+mainWindow.columnconfigure(0, weight=1) #column 0 expands with window
+mainWindow.columnconfigure(1, weight=1) #column 1 expands with window
+mainWindow.grid_columnconfigure(2, weight=1) #column 2 expands with window
+
+leftFrame.config(relief='sunken', borderwidth=1) #add border to frame
+rightFrame.config(relief='sunken', borderwidth=1) #add border to frame
+leftFrame.grid(sticky='ns') #make the frame stick to north and south
+rightFrame.grid(sticky='new') #make the frame stick to all sides
+
+rightFrame.columnconfigure(0, weight=1) #column 0 expands with window   
+button2.grid(sticky='ew') #make the button stick to east and west
 mainWindow.mainloop() # placce window on computer screen, listen for events, wait for user input
 
 
